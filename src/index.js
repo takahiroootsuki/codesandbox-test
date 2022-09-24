5bdb86958d8ec63efc56ad0726f733c9eb57f603
takahiroootsuki <takahiro.ootsuki@jp.joysonsafety.com>
2022-09-24 02:44:54 +0000

    Update 13 files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./styles.css";
-console.log("aaabb");
+console.log("assaabbcssssss");
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
-<p>test</p>
-
+<p>test2</p>
+<p>修正しました</p>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
